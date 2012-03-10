2eb474b8a1f456c627a0fbb05661eecd9ad6d068
Paddy Foran <paddy@secondbit.org>
2012-03-10 05:41:34 -0500

    Fleshed out error types, starting trimming whitespace from auth keys.

--- stripe.go
+++ stripe.go
@@ -2,9 +2,12 @@ package stripe
 
 import (
 	"bytes"
+        "fmt"
 	"io/ioutil"
 	"net/http"
+        "net/url"
 	"strconv"
+        "strings"
 )
 
 var (
@@ -19,19 +22,111 @@ type Stripe struct {
 }
 
 func New(auth string) *Stripe {
-	return &Stripe{HOST, VERSION, auth}
+	return &Stripe{HOST, VERSION, strings.TrimSpace(auth)}
 }
 
 type BadRequestError struct {
+        Message string "message"
+        Request *http.Request "request"
+}
+
+func (err *BadRequestError) Error() string {
+        if err.Request != nil {
+                err.Message = fmt.Sprintf("%v\nRequest: %v", err.Message, err.Request)
+        }
+        return err.Message
+}
+
+func ThrowBadRequest(req *http.Request) *BadRequestError {
+        return &BadRequestError {
+                Message: "Error: Bad request.",
+                Request: req,
+        }
 }
 
 type UnauthorizedError struct {
+        Message string "message"
+        Auth string "auth"
+}
+
+func (err *UnauthorizedError) Error() string {
+        if err.Auth == "" {
+                return "No authorization set."
+        }
+        return fmt.Sprintf("%v\nAuth: %v", err.Message, err.Auth)
+}
+
+func ThrowUnauthorized(req *http.Request) *UnauthorizedError {
+        return &UnauthorizedError {
+                Message: "Error: Unauthorized.",
+                Auth: req.Header["Authorization"][0],
+        }
+}
+
+type RequestFailedError struct {
+        Message string "message"
+        Request *http.Request "request"
+}
+
+func (err *RequestFailedError) Error() string {
+        if err.Request != nil {
+                err.Message = fmt.Sprintf("%v\nRequest: %v", err.Message, err.Request)
+        }
+        return err.Message
+}
+
+func ThrowRequestFailed(req *http.Request) *RequestFailedError {
+        return &RequestFailedError {
+                Message: "Error: Request failed.",
+                Request: req,
+        }
 }
 
 type NotFoundError struct {
+        Message string "message"
+        URL *url.URL "url"
+}
+
+func (err *NotFoundError) Error() string {
+        if err.URL != nil {
+                err.Message = fmt.Sprintf("%v\nURL: %v", err.Message, err.URL)
+        }
+        return err.Message
+}
+
+func ThrowNotFound(req *http.Request) *NotFoundError {
+        return &NotFoundError {
+                Message: "Error: Resource not found.",
+                URL: req.URL,
+        }
 }
 
 type ServerError struct {
+        Message string "message"
+}
+
+func (err *ServerError) Error() string {
+        return err.Message
+}
+
+func ThrowServer(req *http.Request) *ServerError {
+        return &ServerError {
+                Message: "Error: Internal Server Error.",
+        }
+}
+
+type UnknownError struct {
+        Message string "message"
+}
+
+func (err *UnknownError) Error() string {
+        return err.Message
+}
+
+func ThrowUnknown(code int) *UnknownError {
+        return &UnknownError {
+                Message: "Error: Unknown error occurred.\nStatus Code: "+strconv.Itoa(code),
+        }
 }
 
 type RawError struct {
@@ -39,6 +134,10 @@ type RawError struct {
 	Param   string "param"
 	Code    string "code"
 	Message string "message"
+}
+
+func (err *RawError) Error() string {
+        return fmt.Sprintf("Error (%v): %v\nType: %v\nParam: %v", err.Code, err.Message, err.Type, err.Param)
 }
 
 type InvalidRequestError struct {
@@ -96,34 +195,34 @@ func (stripe *Stripe) request(method, url string, body string) (resp []byte, err
 	if err != nil {
 		return nil, err
 	}
-	req.SetBasicAuth(stripe.AuthKey, "")
+	req.SetBasicAuth(stripe.AuthKey,"")
 	hresp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	switch hresp.StatusCode {
 	case 400:
-		// TODO: Throw a BadRequest error
+		return nil, ThrowBadRequest(req)
 	case 401:
-		// TODO: Throw an Unauthorized error
+                return nil, ThrowUnauthorized(req)
 	case 402:
-		// TODO: Throw a RequestFailed error
+		return nil, ThrowRequestFailed(req)
 	case 404:
-		// TODO: Throw a NotFound error
+                return nil, ThrowNotFound(req)
 	case 500:
-		// TODO: Throw a Server error
+                return nil, ThrowServer(req)
 	case 502:
-		// TODO: Throw a Server error
+                return nil, ThrowServer(req)
 	case 503:
-		// TODO: Throw a Server error
+                return nil, ThrowServer(req)
 	case 504:
-		// TODO: Throw a Server error
+                return nil, ThrowServer(req)
 	case 200:
-		// TODO: Keep calm, carry on
+        	resp, err = ioutil.ReadAll(hresp.Body)
+	        hresp.Body.Close()
+        	return resp, err
 	default:
-		// TODO: Throw a generic error
+		return nil, ThrowUnknown(hresp.StatusCode)
 	}
-	resp, err = ioutil.ReadAll(hresp.Body)
-	hresp.Body.Close()
-	return resp, err
+        return nil, ThrowUnknown(hresp.StatusCode)
 }
